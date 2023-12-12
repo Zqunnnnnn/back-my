0b8d011031386152743e1e16660666aefd25c05c
Zqunnnnnn <1092404442@qq.com>
2023-12-12 17:26:11 +0800

    Changes

--- src/main/java/com/example/demo/bean/Emp.java
+++ src/main/java/com/example/demo/bean/Emp.java
@@ -6,6 +6,7 @@ import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import java.io.Serializable;
 import lombok.Data;
+import lombok.ToString;
 
 /**
  *
@@ -13,6 +14,7 @@ import lombok.Data;
  */
 @TableName(value ="emp")
 @Data
+@ToString
 public class Emp implements Serializable {
     /**
      *
--- src/main/java/com/example/demo/controller/EmpController.java
+++ src/main/java/com/example/demo/controller/EmpController.java
@@ -1,5 +1,6 @@
 package com.example.demo.controller;
 
+import cn.hutool.poi.excel.ExcelReader;
 import cn.hutool.poi.excel.ExcelUtil;
 import cn.hutool.poi.excel.ExcelWriter;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
@@ -10,10 +11,12 @@ import com.example.demo.mapper.EmpMapper;
 import com.example.demo.service.impl.EmpServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URLEncoder;
 import java.util.List;
 
@@ -32,7 +35,7 @@ public class EmpController {
     }
     @PostMapping("/updateOrAddEmp")
     public boolean updateOrAddEmp(@RequestBody Emp emp){
-        if(emp.getDeptId()==null){
+        if(emp.getDeptId()==null||emp.getEmpAge()==null||emp.getEmpSex()==null||emp.getEmpName()==null){
             return false;
         }
         return empService.saveOrUpdate(emp);
@@ -103,11 +106,11 @@ public class EmpController {
         //获取所有信息
         List<Emp> list = empService.list();
         ExcelWriter writer = ExcelUtil.getWriter("C:\\Users\\Zqunnnnnn\\Desktop\\study\\java\\demo\\src\\main\\resources\\templates\\export"+"/用户信息.xlsx");
-        writer.addHeaderAlias("empId","员工编号");
-        writer.addHeaderAlias("empName","员工姓名");
-        writer.addHeaderAlias("empSex","性别");
-        writer.addHeaderAlias("empAge","年龄");
-        writer.addHeaderAlias("deptId","部门编号");
+//        writer.addHeaderAlias("empId","员工编号");
+//        writer.addHeaderAlias("empName","员工姓名");
+//        writer.addHeaderAlias("empSex","性别");
+//        writer.addHeaderAlias("empAge","年龄");
+//        writer.addHeaderAlias("deptId","部门编号");
         //将list赋到writer中
         writer.write(list,true);
         //设置浏览器响应的格式
@@ -119,8 +122,15 @@ public class EmpController {
         writer.flush(out, true);
         out.close();
         writer.close();
-
     }
+    @PostMapping("/import")
+    public void importData(MultipartFile file) throws IOException {
+        InputStream inputStream = file.getInputStream();
+        ExcelReader reader = ExcelUtil.getReader(inputStream);
+        List<Emp> emps = reader.readAll(Emp.class);
+        empService.saveBatch(emps);
+    }
+
 
 
 }
